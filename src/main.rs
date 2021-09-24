289d6645780ce60f94cc966cca0fcf4a14d8e174
Paweł Jarosz <jarosz.pawel@outlook.com>
2021-09-24 19:21:13 +0200

    Ignore files taken from excluded directories

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
 use std::path::Path;
 use std::fs;
+use std::fmt;
+use yaml_rust::YamlLoader;
+
+struct ProjectConfiguration {
+    root_dir: String,
+    excluded_dirs: Vec<String>
+}
+
+impl ProjectConfiguration {
+
+fn new(filename: &str) -> ProjectConfiguration {
+    let s =
+    "
+    root_dir: /home/caedus/Repos/sample-applications/cm_provider_subscriber_scenario
+    excluded_dirs:
+        - radar/
+    ";
+    let parsed_config = &YamlLoader::load_from_str(s).unwrap()[0];
+    let mut excluded_dirs = Vec::new();
+    for item in parsed_config["excluded_dirs"].as_vec().expect("Invalid excluded dirs list") {
+        excluded_dirs.push(String::from(item.as_str().expect("Invalid excluded path")));
+    }
+    ProjectConfiguration {
+        root_dir: String::from(parsed_config["root_dir"].as_str().expect("Invalid root directory")),
+        excluded_dirs: excluded_dirs
+    }
+}
+
+}
 
 fn is_arxml(filename: &str) -> bool {
     if filename.ends_with(".arxml") {
@@ -8,7 +37,17 @@ fn is_arxml(filename: &str) -> bool {
     false
 }
 
-fn find_arxml(path: &Path, result: &mut Vec<String>) {
+fn is_excluded_file(filename: &str, configuration: &ProjectConfiguration) -> bool {
+    for item in &configuration.excluded_dirs {
+        if filename.starts_with(&format!("{}/{}", configuration.root_dir, item)) {
+            return true;
+        }
+    }
+    false
+}
+
+
+fn find_arxml(path: &Path, is_excluded: &impl Fn(&str) -> bool, result: &mut Vec<String>) {
     if !path.is_dir() {
         return;
     }
@@ -16,11 +55,11 @@ fn find_arxml(path: &Path, result: &mut Vec<String>) {
     for item in entry {
         let path = item.expect("Invalid path").path();
         if path.as_path().is_dir() {
-            find_arxml(path.as_path(), result);
+            find_arxml(path.as_path(), is_excluded, result);
         }
         else {
             let filename = path.to_str().expect("Invalid string");
-            if is_arxml(filename) {
+            if is_arxml(filename) && !is_excluded(filename) {
                 result.push(filename.to_string());
             }
         }
@@ -28,8 +67,10 @@ fn find_arxml(path: &Path, result: &mut Vec<String>) {
 }
 
 fn main() {
+    let config = ProjectConfiguration::new("dupa");
     let mut arxml_files = Vec::new();
-    find_arxml(Path::new("/home/caedus/Repos/sample-applications/cm_provider_subscriber_scenario"), &mut arxml_files);
+    let is_excluded = |filename: &str| is_excluded_file(filename, &config);
+    find_arxml(Path::new("/home/caedus/Repos/sample-applications/cm_provider_subscriber_scenario"), &is_excluded, &mut arxml_files);
     for item in arxml_files {
         println!("{}", item)
     }
