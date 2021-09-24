61fb42bd67fe6b226ceb5ee1dca1ff85e16502ca
Paweł Jarosz <jarosz.pawel@outlook.com>
2021-09-24 18:25:32 +0200

    Add finding arxml files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::path::Path;
+use std::fs;
+
+fn is_arxml(filename: &str) -> bool {
+    if filename.ends_with(".arxml") {
+        return true;
+    }
+    false
+}
+
+fn find_arxml(path: &Path, result: &mut Vec<String>) {
+    if !path.is_dir() {
+        return;
+    }
+    let entry = fs::read_dir(path).expect("Invalid path");
+    for item in entry {
+        let path = item.expect("Invalid path").path();
+        if path.as_path().is_dir() {
+            find_arxml(path.as_path(), result);
+        }
+        else {
+            let filename = path.to_str().expect("Invalid string");
+            if is_arxml(filename) {
+                result.push(filename.to_string());
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut arxml_files = Vec::new();
+    find_arxml(Path::new("/home/caedus/Repos/sample-applications/cm_provider_subscriber_scenario"), &mut arxml_files);
+    for item in arxml_files {
+        println!("{}", item)
+    }
 }
